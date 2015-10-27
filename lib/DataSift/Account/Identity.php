e60570e5419df68784832f5684f717c22be73e46
Christopher Hoult <chris.hoult@datasift.com>
2015-10-27 15:30:14 +0000

    Passing label to the GET /account/identity list endpoint

--- lib/DataSift/Account/Identity.php
+++ lib/DataSift/Account/Identity.php
@@ -27,19 +27,19 @@ class DataSift_Account_Identity extends DataSift_Account
 {
     /**
      * Returns an identity
-     * 
+     *
      * @param string $identity
-     * 
+     *
      * @return mixed
      */
     public function get($identity)
-    {        
+    {
         return $this->_user->get('account/identity/'. $identity);
     }
 
     /**
      * Gets all the identities
-     * 
+     *
      * @param string    $label
      * @param integer   $page
      * @param integer   $perPage
@@ -51,17 +51,21 @@ class DataSift_Account_Identity extends DataSift_Account
             'page' => $page,
             'per_page' => $perPage
         );
-        
+
+        if ($label !== null) {
+            $params['label'] = $label;
+        }
+
         return $this->_user->get('account/identity', $params);
     }
-    
+
     /**
      * Creates an identity
-     * 
+     *
      * @param string    $label
      * @param string    $master
      * @param string    $status
-     * 
+     *
      * @return mixed
      */
     public function create($label, $master = false, $status = 'active')
@@ -72,23 +76,23 @@ class DataSift_Account_Identity extends DataSift_Account
             'master'    => $master,
             'status'    => $status
         );
-        
+
         return  $this->_user->post('account/identity', $params);
     }
-    
+
     /**
      * Updates an identity
-     * 
+     *
      * @param string    $identity
      * @param string    $master
      * @param string    $label
      * @param string    $status
-     * 
+     *
      * @return mixed
      */
-    public function update($identity, $label = null, $master = null, $status = null) 
+    public function update($identity, $label = null, $master = null, $status = null)
     {
-        
+
         $params = array();
         if (!is_null($label)) {
             $params['label'] = $label;
@@ -99,15 +103,15 @@ class DataSift_Account_Identity extends DataSift_Account
         if (!is_null($status)) {
             $params['status'] = $status;
         }
-        
+
         return $this->_user->put('account/identity/' . $identity, $params);
     }
-    
+
     /**
      * Deletes an identity
-     * 
+     *
      * @param string    $identity
-     * 
+     *
      * @return mixed
      */
     public function delete($identity)
